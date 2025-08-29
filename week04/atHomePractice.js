136fb40329e7ee7436802efe24ee570ba17b24a2
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-08-29 23:05:57 +0700

    More Practice at Home about forEach and Object (29 Aug 2025)

--- week04/atHomePractice.js
+++ week04/atHomePractice.js
@@ -18,4 +18,27 @@ const total = multiplied.reduce((acc, num) => acc + num, 0);
 
 console.log("Even numbers:", evens);       
 console.log("After *3:", multiplied);     
-console.log("Total sum:", total);          
+console.log("Total sum:", total);  
+console.log ("========================================")      
+
+
+/*Given the object:
+const students = {
+  Ann: 80,
+  Bob: 65,
+  Charlie: 90,
+};
+Use forEach() (after converting the object into an array with Object.entries) to print the following:
+    Ann has 80 points
+    Bob has 65 points
+    Charlie has 90 points */
+const students = {
+  Ann: 80,
+  Bob: 65,
+  Charlie: 90,
+};
+const entries = Object.entries(students); // แปลง object เป็น array ของ [key, value]
+
+entries.forEach(([name, score]) => {
+  console.log(`${name} has ${score} points`);
+});
